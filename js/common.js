5cc9d783b807fde2ff8bdb3170888ca850babd8c
Noohkoess <shs9427.git@gmail.com>
2024-03-13 17:11:07 +0900

    1차

---
+++ js/common.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
